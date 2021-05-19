7353d562877c08051a265b667f5d27a7d85566b2
Bruno Wedley <brunowedley27@gmail.com>
2021-05-19 12:34:50 -0300

    modificação para início de novo portfólio

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar.js"
+
+function App() {
+  return (
+    <Navbar />
+  );
+}
+
+export default App;
